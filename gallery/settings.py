31a75f487fb95ca0d15b9e032fd0f65e2e2b5d06
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-28 09:28:08 -0500

    import cloudinary

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
