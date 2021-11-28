12b34ec1d7e216114544a745c6ac2d06dc57a507
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-28 09:21:14 -0500

    add cloudinary to installed apps

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'images',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4'
+    'bootstrap4',
+    'images.apps.ImagesConfig',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
