a32ebc3c90f971cba2e5f53a5c203203ad02f35e
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-28 09:01:46 -0500

    add images url

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^news/',include('news.urls'))
+    url(r'^images/',include('images.urls'))
 
 ]
