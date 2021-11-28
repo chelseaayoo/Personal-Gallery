fd9c47daa0d34deb5658b82b4ec2311b4f7950f8
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-28 07:39:33 -0500

    update database

--- gallery/settings.py
+++ gallery/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'photos',
-        'USER': '<anipherchelsea >',
-    'PASSWORD':'<Onyango09>',
+        'USER': 'anipherchelsea',
+    'PASSWORD':'Onyango09',
     }
 }
 
