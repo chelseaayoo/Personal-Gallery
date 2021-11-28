ad16a55072636416c9c91db15ec47a087f1802a6
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-28 10:13:06 -0500

    update url pattern to automatically load the app

--- images/urls.py
+++ images/urls.py
@@ -1,6 +1,14 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import url 
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.urls import path
 
-urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+
+urlpatterns = [
+    url(r'^search/', views.search, name='search_results'),
+    url(r'^$',views.welcome,name='index'),
+    url(r'^location/',views.location,name='location'),
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
