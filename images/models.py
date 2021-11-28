a3ff88c6523a6bd5d7508e419141d8609044e901
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-28 07:55:04 -0500

    Create image model

--- images/models.py
+++ images/models.py
@@ -25,3 +25,40 @@ class Category(models.Model):
 
     def delete_category(self):
         self.delete()
+
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    loc =  models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    # image = CloudinaryField('image')
+
+    def __str__(self):
+        return self.image.url
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls,image_id):
+        image = cls.objects.get(id=image_id)
+        return image
+
+    @classmethod
+    def search_image(cls,category):
+        images = cls.objects.filter(category__name__icontains=category).all()
+        return images
+    
+    @classmethod
+    def filter_by_location(cls,location):
+        images = cls.objects.filter(loc__name=location).all()
+        return images
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
