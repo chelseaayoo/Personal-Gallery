282ec54e1b9340bc6b6d992b24f27d6b98388d62
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-29 06:28:28 -0500

    add media url

--- gallery/settings.py
+++ gallery/settings.py
@@ -132,6 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
