7e36d056441b5207309e1c3404e7b5480ed5b251
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-29 06:45:02 -0500

    create get location view function

--- images/views.py
+++ images/views.py
@@ -1,42 +1,57 @@
-from django.shortcuts import render
-from django.http import Http404
-from .models import Location,Category,Image
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Category, Image, Location
+from django.core.checks import messages
 from django.core.exceptions import ObjectDoesNotExist
 
+
 # Create your views here.
-
-
 def welcome(request):
-    images = Image.all_images()
-    return render (request,'index.html',{"images":images})
-
-def search(request):
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Image.search_image(search_term)
-        message = f"{search_term}"
-
-        return render(request,"search.html",{"message":message,"images":searched_images})
-
-    else:
-        message = "You haven't searched for any category"
-        return render(request,"search.html",{"message":message})
-
-def image(request,image_id):
+    categories = Category.objects.all()
     try:
-        image = Image.get_image_by_id(image_id)
-
+        photos = Image.objects.all()
     except ObjectDoesNotExist:
         raise Http404()
-    return render(request,"image.html",{"image":image})
+    
+    return render(request,'index.html',{'photos':photos,'categories':categories})
 
-def location(request):
-    if 'location' in request.GET and request.GET["location"]:
-        location = request.GET.get("location")
-        searched_images = Image.filter_by_location(location)
-        message = f"{location}"
-        return render(request,"search.html",{"message":message,"images":searched_images})
+def gallery_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
 
+def get_photo(request):
+    try:
+        photos = Image.objects.all()
+    except ObjectDoesNotExist:
+        raise Http404()
+    # import pdb; pdb.set_trace()
+    return render(request, 'image.html', {"photos":photos})
+
+def get_category(request):
+    category = Category.object.all()
+
+    return render(request,'index.html', {"category":category})
+ 
+def search_results(request):
+    if 'photo' in request.GET and request.GET["photos"]:
+        search_term = request.GET.get("photo")
+        searched_photos = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
     else:
-        message = "select location to filter"
-        return render(request,"search.html",{"message":message})
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+def get_location(request):
+    location = Location.object.all()
+
+    return render(request,'index.html', 'base.html','image.html','navbar.html',{"location":location})
